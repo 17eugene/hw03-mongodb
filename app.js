21490c115f6f6782854ea63dfb53ffe6580206f9
17eugene <drivercoua@gmail.com>
2021-11-07 00:18:49 +0200

    add .env

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const DB_HOST = process.env.DB_HOST;
+console.log(DB_HOST);
 
 const contactsRouter = require("./routes/api/contacts");
 
